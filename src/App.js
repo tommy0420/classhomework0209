104b1447605578569c01da7b29d0af0c1308fdb2
富井涼介 <rtbaseballandlionslove@i.softbank.jp>
2020-02-12 00:08:36 +0900

    0209 react授業課題

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Form from "./Form"
+import List from "./List"
+
+const App = () => {
+    return (
+        <div>
+            <Form />
+            <List />
+        </div>
+    )
+}
+
+export default App
---
+++ src/Form.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Form = () => {
+    return (
+        <form action="#">
+            <input type="text" />
+            <button type="button"></button>
+        </form>
+    )
+}
+
+export default Form
